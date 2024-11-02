c23334fe663dddf775c6d2dd45dd855e2187168c
mxg-mega <68956501+mxg-mega@users.noreply.github.com>
2024-11-02 10:18:51 +0100

    Initial Commit
    I am using this repos to learn the basics of C#

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int num1 = 0;
+        int num2 = 0;
+
+        Console.WriteLine("Basic Calculator");
+        int sum = num1 + num2;
+        int difference = num1 + num2;
+        int multiplicataion = num1 + num2;
+        int division = num1 + num2;
+        int modulus = num1 + num2;
+    }
+}
